41ba1434653f14cdb9d86a4bbe0804e2d51eefed
Rolando Alvarado <rorroland@gmail.com>
2017-11-07 23:32:58 +0800

    Added Benchmarking

--- app/services/file_services/import.rb
+++ app/services/file_services/import.rb
@@ -4,6 +4,8 @@ require 'csv'
 
 module FileServices
   class Import
+    include Benchmarking
+
     INVALID_FILE_TYPE = 'Invalid File type.'
     INVALID_CSV_TYPE = 'Invalid CSV type.'
     SUCCESSFUL_IMPORT = 'File has been imported successfully.'
@@ -37,29 +39,34 @@ module FileServices
       csv_type = @attrs[:csv_type].first
       rows = []
       model_name = csv_type.constantize
-      CSV.foreach(@attrs[:file].path, headers: true) do |row|
-        row_hash = row.to_h
-        case csv_type
-        when 'Dish'
-          rows << model_name.new(row_hash) if row_hash['id'] && row_hash['name'].present?
-        when 'Menu'
-          unless row_hash['id'].nil? && row_hash['sponsor'].nil? &&
-            row_hash['event'].nil? && row_hash['venue'].nil? && row_hash['place'].nil?
-            rows << model_name.new(row_hash)
-          end
-        when 'MenuItem'
-          unless row_hash['id'].nil? && row_hash['menu_page_id'].nil? &&
-            row_hash['dish_id'].nil? && row_hash['price'].nil?
-            rows << model_name.new(row_hash)
-          end
-        when 'MenuPage'
-          unless row_hash['id'].nil? && row_hash['menu_id'].nil?
-            rows << model_name.new(row_hash)
-          end
-        else
-          return { status: :unprocessable_entity, message: INVALID_CSV_TYPE }
-        end
-      end
+      print_memory_usage do
+        print_time_spent do
+          CSV.foreach(@attrs[:file].path, headers: true) do |row|
+            row_hash = row.to_h
+            case csv_type
+            when 'Dish'
+              rows << model_name.new(row_hash) if row_hash['id'] && row_hash['name'].present?
+            when 'Menu'
+              unless row_hash['id'].nil? && row_hash['sponsor'].nil? &&
+                row_hash['event'].nil? && row_hash['venue'].nil? && row_hash['place'].nil?
+                rows << model_name.new(row_hash)
+              end
+            when 'MenuItem'
+              unless row_hash['id'].nil? && row_hash['menu_page_id'].nil? &&
+                row_hash['dish_id'].nil? && row_hash['price'].nil?
+                rows << model_name.new(row_hash)
+              end
+            when 'MenuPage'
+              unless row_hash['id'].nil? && row_hash['menu_id'].nil?
+                rows << model_name.new(row_hash)
+              end
+            else
+              return { status: :unprocessable_entity, message: INVALID_CSV_TYPE }
+            end
+          end # /CSV
+        end # /print_time_spent
+      end # /print_memory_usage
+
       model_name.import(rows)
       return { status: :ok, message: SUCCESSFUL_IMPORT }
     rescue => error
